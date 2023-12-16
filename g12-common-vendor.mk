6ef9c7cf70432862fe79ff4a888de06e6df98b62
Bruno Martins <bgcngm@gmail.com>
2023-12-16 14:50:30 +0000

    g12: Move kernel modules to vendor DLKM partition
    Change-Id: I2e99c6fa6795fbd0f16a909fdb2e171c1af40158

--- g12-common-vendor.mk
+++ g12-common-vendor.mk
@@ -105,7 +105,7 @@ PRODUCT_COPY_FILES += \
     vendor/amlogic/g12-common/proprietary/vendor/lib/libstagefrighthw.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefrighthw.so \
     vendor/amlogic/g12-common/proprietary/vendor/lib/libthreadworker_alt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libthreadworker_alt.so \
     vendor/amlogic/g12-common/proprietary/vendor/lib/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwvhidl.so \
-    vendor/amlogic/g12-common/proprietary/vendor/lib/modules/amlogic_fbc_lib.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules/amlogic_fbc_lib.ko \
+    vendor/amlogic/g12-common/proprietary/vendor_dlkm/lib/modules/amlogic_fbc_lib.ko:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/amlogic_fbc_lib.ko \
     vendor/amlogic/g12-common/proprietary/vendor/lib/soundfx/libavl.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libavl.so \
     vendor/amlogic/g12-common/proprietary/vendor/lib/soundfx/libbalance.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libbalance.so \
     vendor/amlogic/g12-common/proprietary/vendor/lib/soundfx/libdbx.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdbx.so \
