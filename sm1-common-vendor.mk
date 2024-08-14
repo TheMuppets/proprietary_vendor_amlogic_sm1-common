b6104e9a1d878d3e1284f1e32c0998d4453ca3c8
Aaron Kling <webgeek1234@gmail.com>
2024-08-14 01:44:45 -0500

    sm1: Enable elf checks
    Change-Id: Ia6c9a381abb3384c77532d75e02fd84c841a4d1e

--- sm1-common-vendor.mk
+++ sm1-common-vendor.mk
@@ -8,20 +8,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     vendor/amlogic/sm1-common/proprietary/product/etc/permissions/privapp-permissions-atv-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-atv-product.xml \
     vendor/amlogic/sm1-common/proprietary/product/etc/permissions/privapp-permissions-deadpool.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-deadpool.xml \
-    vendor/amlogic/sm1-common/proprietary/recovery/root/system/bin/systemcontrol_static:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/systemcontrol_static \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/DecInfo_test:$(TARGET_COPY_OUT_VENDOR)/bin/DecInfo_test \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/ddrtest.sh:$(TARGET_COPY_OUT_VENDOR)/bin/ddrtest.sh \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/esplayer:$(TARGET_COPY_OUT_VENDOR)/bin/esplayer \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/hdcp_tx22:$(TARGET_COPY_OUT_VENDOR)/bin/hdcp_tx22 \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/hw/android.hardware.bluetooth@1.0-service-droidlogic:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.bluetooth@1.0-service-droidlogic \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/hw/android.hardware.drm@1.4-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.4-service.widevine \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/hw/android.hardware.dumpstate@1.1-service.droidlogic:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.dumpstate@1.1-service.droidlogic \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/hw/android.hardware.graphics.allocator@4.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.graphics.allocator@4.0-service \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/hw/android.hardware.graphics.composer@2.4-service.droidlogic:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.graphics.composer@2.4-service.droidlogic \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/hw/android.hardware.thermal@2.0-service.droidlogic:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.thermal@2.0-service.droidlogic \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/hw/android.hardware.usb.gadget@1.2-service.droidlogic:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.usb.gadget@1.2-service.droidlogic \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/remotecfg:$(TARGET_COPY_OUT_VENDOR)/bin/remotecfg \
-    vendor/amlogic/sm1-common/proprietary/vendor/bin/systemcontrol:$(TARGET_COPY_OUT_VENDOR)/bin/systemcontrol \
     vendor/amlogic/sm1-common/proprietary/vendor/etc/bluetooth/auto_pairing.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/auto_pairing.conf \
     vendor/amlogic/sm1-common/proprietary/vendor/etc/bluetooth/blacklist.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/blacklist.conf \
     vendor/amlogic/sm1-common/proprietary/vendor/etc/bluetooth/bt_vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_vendor.conf \
@@ -54,93 +40,106 @@ PRODUCT_COPY_FILES += \
     vendor/amlogic/sm1-common/proprietary/vendor/etc/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
     vendor/amlogic/sm1-common/proprietary/vendor/etc/wifi/p2p_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant.conf \
     vendor/amlogic/sm1-common/proprietary/vendor/etc/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/android.hardware.graphics.composer@2.1-resources-vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.graphics.composer@2.1-resources-vendor.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/android.hardware.graphics.composer@2.2-resources-vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.graphics.composer@2.2-resources-vendor.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/arm.graphics-V1-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib/arm.graphics-V1-ndk_platform.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/egl/libGLES_mali.so:$(TARGET_COPY_OUT_VENDOR)/lib/egl/libGLES_mali.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/hw/android.hardware.bluetooth@1.0-impl-droidlogic.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/android.hardware.bluetooth@1.0-impl-droidlogic.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/hw/android.hardware.graphics.allocator@4.0-impl-arm.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/android.hardware.graphics.allocator@4.0-impl-arm.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/hw/android.hardware.graphics.mapper@4.0-impl-arm.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/android.hardware.graphics.mapper@4.0-impl-arm.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/hw/audio.primary.amlogic.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/audio.primary.amlogic.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/hw/camera.amlogic.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/camera.amlogic.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/hw/hdmi_cec.amlogic.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/hdmi_cec.amlogic.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/hw/hwcomposer.amlogic.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/hwcomposer.amlogic.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/hw/memtrack.amlogic.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/memtrack.amlogic.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/hw/screen_source.amlogic.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/screen_source.amlogic.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libAmlAudioOutPort.so:$(TARGET_COPY_OUT_VENDOR)/lib/libAmlAudioOutPort.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libOmxAudio.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOmxAudio.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libOmxBase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOmxBase.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libOmxCore.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOmxCore.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libOmxVideo.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOmxVideo.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libam_adp.so:$(TARGET_COPY_OUT_VENDOR)/lib/libam_adp.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamadec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamadec.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamaudioutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamaudioutils.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamavutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamavutils.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamgralloc_ext.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamgralloc_ext.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamladecs.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamladecs.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamlaudiohal.7.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamlaudiohal.7.0.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamlaudiorc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamlaudiorc.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamlparser.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamlparser.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamlresampler.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamlresampler.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamlspeed.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamlspeed.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libamltinyalsa.so:$(TARGET_COPY_OUT_VENDOR)/lib/libamltinyalsa.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libdatachunkqueue_alt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdatachunkqueue_alt.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libdroidaudiospdif.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdroidaudiospdif.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libdsm.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdsm.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libdvbaudioutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libdvbaudioutils.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libfaad.so:$(TARGET_COPY_OUT_VENDOR)/lib/libfaad.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libfbc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libfbc.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libfpscalculator_alt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libfpscalculator_alt.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libge2d.so:$(TARGET_COPY_OUT_VENDOR)/lib/libge2d.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libicuuc_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib/libicuuc_vendor.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libispaaa.so:$(TARGET_COPY_OUT_VENDOR)/lib/libispaaa.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libmad.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmad.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libmediahal_mediasync.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmediahal_mediasync.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libmediahal_passthrough.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmediahal_passthrough.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libmediahal_resman.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmediahal_resman.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libmediahal_tsplayer.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmediahal_tsplayer.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libmediahal_tunnelrenderer.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmediahal_tunnelrenderer.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libmediahal_videodec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmediahal_videodec.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libmeson_display_adapter_remote.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmeson_display_adapter_remote.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libmeson_display_service.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmeson_display_service.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libms12api_v24.so:$(TARGET_COPY_OUT_VENDOR)/lib/libms12api_v24.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libnano.so:$(TARGET_COPY_OUT_VENDOR)/lib/libnano.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libomx_av_core_alt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libomx_av_core_alt.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libomx_clock_utils_alt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libomx_clock_utils_alt.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libomx_framework_alt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libomx_framework_alt.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libomx_timed_task_queue_alt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libomx_timed_task_queue_alt.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libomx_worker_peer_alt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libomx_worker_peer_alt.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libpqcontrol.so:$(TARGET_COPY_OUT_VENDOR)/lib/libpqcontrol.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libsonic_ext.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsonic_ext.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libstagefrighthw.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefrighthw.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libsystemcontrolservice.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsystemcontrolservice.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libthreadworker_alt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libthreadworker_alt.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libvideotunnel.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvideotunnel.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwvhidl.so \
-    vendor/amlogic/sm1-common/proprietary/vendor_dlkm/lib/modules/amlogic_fbc_lib.ko:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/amlogic_fbc_lib.ko \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/soundfx/libavl.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libavl.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/soundfx/libbalance.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libbalance.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/soundfx/libdbx.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdbx.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/soundfx/libhpeqwrapper.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libhpeqwrapper.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/soundfx/libms12dapwrapper.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libms12dapwrapper.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/soundfx/libsrswrapper.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libsrswrapper.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/soundfx/libtreblebasswrapper.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libtreblebasswrapper.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/soundfx/libvirtualsurround.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libvirtualsurround.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/soundfx/libvirtualx.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libvirtualx.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/vendor.amlogic.display.meson_display_ipc@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.amlogic.display.meson_display_ipc@1.0.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/vendor.amlogic.hardware.droidvold@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.amlogic.hardware.droidvold@1.0.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/vendor.amlogic.hardware.systemcontrol@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.amlogic.hardware.systemcontrol@1.0.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/vendor.amlogic.hardware.systemcontrol@1.1.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.amlogic.hardware.systemcontrol@1.1.so \
-    vendor/amlogic/sm1-common/proprietary/vendor/lib/vendor.amlogic.hardware.tvserver@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.amlogic.hardware.tvserver@1.0.so
+    vendor/amlogic/sm1-common/proprietary/vendor_dlkm/lib/modules/amlogic_fbc_lib.ko:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/amlogic_fbc_lib.ko
 
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-resources-vendor \
+    android.hardware.graphics.composer@2.2-resources-vendor \
+    arm.graphics-V1-ndk_platform \
+    libGLES_mali \
+    android.hardware.bluetooth@1.0-impl-droidlogic \
+    android.hardware.graphics.allocator@4.0-impl-arm \
+    android.hardware.graphics.mapper@4.0-impl-arm \
+    audio.primary.amlogic \
+    camera.amlogic \
+    hdmi_cec.amlogic \
+    hwcomposer.amlogic \
+    memtrack.amlogic \
+    screen_source.amlogic \
+    libAmlAudioOutPort \
+    libOmxAudio \
+    libOmxBase \
+    libOmxCore \
+    libOmxVideo \
+    libam_adp \
+    libamadec \
+    libamaudioutils \
+    libamavutils \
+    libamgralloc_ext \
+    libamladecs \
+    libamlaudiohal.7.0 \
+    libamlaudiorc \
+    libamlparser \
+    libamlresampler \
+    libamlspeed \
+    libamltinyalsa \
+    libdatachunkqueue_alt \
+    libdroidaudiospdif \
+    libdsm \
+    libdvbaudioutils \
+    libfaad \
+    libfbc \
+    libfpscalculator_alt \
+    libge2d \
+    libicuuc_vendor \
+    libispaaa \
+    libmad \
+    libmediahal_mediasync \
+    libmediahal_passthrough \
+    libmediahal_resman \
+    libmediahal_tsplayer \
+    libmediahal_tunnelrenderer \
+    libmediahal_videodec \
+    libmeson_display_adapter_remote \
+    libmeson_display_service \
+    libms12api_v24 \
+    libnano \
+    libomx_av_core_alt \
+    libomx_clock_utils_alt \
+    libomx_framework_alt \
+    libomx_timed_task_queue_alt \
+    libomx_worker_peer_alt \
+    libpqcontrol \
+    libsonic_ext \
+    libstagefrighthw \
+    libsystemcontrolservice \
+    libthreadworker_alt \
+    libvideotunnel \
+    libwvhidl \
+    libavl \
+    libbalance \
+    libdbx \
+    libhpeqwrapper \
+    libms12dapwrapper \
+    libsrswrapper \
+    libtreblebasswrapper \
+    libvirtualsurround \
+    libvirtualx \
+    vendor.amlogic.display.meson_display_ipc@1.0 \
+    vendor.amlogic.hardware.droidvold@1.0 \
+    vendor.amlogic.hardware.systemcontrol@1.0 \
+    vendor.amlogic.hardware.systemcontrol@1.1 \
+    vendor.amlogic.hardware.tvserver@1.0 \
     Adt3LauncherCustomization \
     DeadpoolService \
     android.hardware.dumpstate@1.1-service.droidlogic.xml \
     android.hardware.graphics.composer@2.4-service.droidlogic.xml \
     android.hardware.thermal@2.0-service.droidlogic.xml \
     android.hardware.usb.gadget@1.2-service.droidlogic.xml \
-    manifest_android.hardware.drm@1.4-service.widevine.xml
+    manifest_android.hardware.drm@1.4-service.widevine.xml \
+    DecInfo_test \
+    ddrtest \
+    esplayer \
+    hdcp_tx22 \
+    android.hardware.bluetooth@1.0-service-droidlogic \
+    android.hardware.drm@1.4-service.widevine \
+    android.hardware.dumpstate@1.1-service.droidlogic \
+    android.hardware.graphics.allocator@4.0-service \
+    android.hardware.graphics.composer@2.4-service.droidlogic \
+    android.hardware.thermal@2.0-service.droidlogic \
+    android.hardware.usb.gadget@1.2-service.droidlogic \
+    remotecfg \
+    systemcontrol
 
 PRODUCT_PACKAGES += \
     libGLES_mali_vulkan.amlogic_symlink32
